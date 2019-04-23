293c1985b2744c1e091425b6fa628652fbc24bf6
caosong <caosong1@yonyou.com>
2019-04-23 20:39:34 +0800

    除数据对比外的其他功能

--- test/trans/gateway/qrcode/unionMainSweep.py
+++ test/trans/gateway/qrcode/unionMainSweep.py
@@ -1,49 +1,23 @@
 import json
-import os
 import unittest
 
 import requests
 
 from facade.enum.db import DBName, DBENV
-from test.common.paramUtil import is_keyword
-from util import DBUtil, ExcelUtil, WorkspaceUtil
+from test.common.paramUtil import is_keyword, analysis_param
+from util import DBUtil, ExcelUtil, WorkspaceUtil, MapUtil, SignUtil
 
 
 # 3.9、二维码主扫消费
-# 请求方式：HTTP POST + JSON
-# 接口地址：/gateway/channel/api/qrCode/mainSweep
-# 请求参数:
-# {
-#   accUsrNo : String,     //接入用户编号
-#   trxChnNo : String,     //交易通道编号
-#   trxTyp : String,       //交易类型代码
-#   trxId : String,        //交易流水号
-#   trxAmt : String,       //交易金额（格式：0.00）
-#   mrchntNo : String,     //商户编号
-#   mrchntNm : String,     //商户名称
-#   productDesc：String     // 商品描述
-#   sign : String          //签名
-# }
-# 返回参数：
-# {
-#   code : String,         //接口返回码，000000 成功
-#   message : String,      //接口返回描述
-#   data : {
-#       rpCd : String,      //交易应答码，000000 成功
-#       rpDesc : String,    //交易应答描述
-#       qrCodeInfo : String //二维码信息
-#   }
-# }
-
 class UnionMainSweepTest(unittest.TestCase):
     conn = None
     cur = None
     # { 1:{'request': '{"accUserNo":"4000","trxChnNo":"2100","trxTyp":"1201","trxId":"17b80f1d1f75adeb","trxAmt":0.01,"mrchntNo":"__createRadomUuid(16)__","mrchntNm":"刘无媚-页面新增企业商户","productDesc":"自动化测试用例1"}',
     # 'predict_response': '{"code":"000000","message":"SUCCESS","data":{"rpCd":"000000","rpDesc":"SUCCESS","qrCodeInfo":"__checkStartWith(https://)__"}}',
-    # 'predict_db_data': '',
-    # 'response': '',
-    # 'db_data': '',
-    # 'start_params': '{"db_search_delay_seconds":0}',
+    # 'predict_db_data': {'statusCode': '200', 'data': {'code': '000000', 'message': '__checkNotEmpty()__', 'data': {'rpCd': '000000', 'rpDesc': '__checkNotEmpty()__', 'qrCodeInfo': '__checkStartWith(https://)__'}}},
+    # 'response': {},
+    # 'db_data': {},
+    # 'start_params': '{"channelKey":"0123456789ABCDEFFEDCBA9876543210"}',
     # 'id': 1}}
     test_data = {}
     excel_path_name = '/resource/testdata/trans/gateway/qrcode/unionMainSweep.xlsx'
@@ -60,18 +34,22 @@ class UnionMainSweepTest(unittest.TestCase):
         self.test_data = self.load_test_data()
 
     # 测试流程开始
-    # 1. 接口访问
-    # 2. 获取接口访问结果
-    # 3. 获取数据库相关数据
-    # 4. 接口返回数据与预测数据对比
-    # 5. 数据库真实数据与预测数据对比
+    # 1. 接口访问 并 获取接口访问结果
+    # 2. 获取数据库相关数据
+    # 3. 接口返回数据与预测数据对比
+    # 4. 数据库真实数据与预测数据对比
     def test_flow(self):
         self.accessInterface()
+        self.get_db_result()
+        self.predictResponse()
 
     def tearDown(self):
         print("tear_down is Running")
-        self.cur.close()
-        self.conn.close()
+        print(self.test_data)
+        if self.cur is not None:
+            self.cur.close()
+        if self.conn is not None:
+            self.conn.close()
 
     # 加载测试数据
     def load_test_data(self):
@@ -84,11 +62,18 @@ class UnionMainSweepTest(unittest.TestCase):
             request = json.loads(each['request'])
             each['predict_response'] = json.loads(each['predict_response'])
             each['start_params'] = json.loads(each['start_params'])
+            each['predict_db_data'] = json.loads(each['predict_db_data'])
+            each['response'] = {}
+            each['db_data'] = {}
             # 请求参数自定义方法调用
             for key, value in request.items():
                 if is_keyword(value):
-                    request[key] = a(value)
+                    request[key] = analysis_param(value)
             each['request'] = request
+            # 签名
+            sign = SignUtil.sign_api_channel_map(each['start_params']['channelKey'],
+                                                 MapUtil.to_tree_map(each['request']))
+            each['request']['sign'] = sign
             self.test_data[each['id']] = each
         print("测试用例:", self.test_data)
         print("【%s】测试用例加载完毕" % self.test_name)
@@ -98,33 +83,37 @@ class UnionMainSweepTest(unittest.TestCase):
     def get_db_result(self, delay_second):
         self.conn = DBUtil.get_connection(DBName.CHANJET_PAY_CHANNEL, DBENV.STABLE)
         self.cur = self.conn.cursor()
-        for index, value in enumerate(self.test_data):
-            self.cur.execute(r"select * from cp_chn_pay_trans where trans_no = '%s'" % (value['trxId']))
+        for key, value in self.test_data.items():
+            self.cur.execute(r"select * from cp_chn_pay_trans where trans_no = '%s'" % (value['request']['trxId']))
             each_result = self.cur.fetchall()
-            self.db_data.append(each_result)
-        print(self.db_data)
+            self.test_data[key]['db_data']['channel'] = each_result
 
-
+    # 访问接口
     def accessInterface(self):
-        for key, value in self.test_data:
+        for key, value in self.test_data.items():
             # 发送请求
-            resp = requests.post(url=self.url, json=json.dumps(value['request']))
-            # 下面是响应的内容
-            content = resp.text
+            resp = requests.post(url=self.url, json=value['request'])
+            # 获取接口访问状态
+            self.test_data[key]['response']['statusCode'] = resp.status_code
+            # 获取响应的内容
+            self.test_data[key]['response']['data'] = resp.text
+        return self.test_data
+
+    # 对比返回结果
+    def compareResponse(self):
+        for key, value in self.test_data.items():
+            real_resp = value['response']
+            pre_resp = value['predict_response']
+            self.assertMultiLineEqual(real_resp['statusCode'], pre_resp['statusCode'], '接口访问状态测试未通过')
+            if pre_resp['data'] is None:
+                self.assertIsNone(real_resp['data'], '接口返回[data]测试不通过')
+            else:
+                real_data = real_resp['data']
+                pre_resp = pre_resp['data']
+                self.assertMultiLineEqual(real_data['code'], pre_resp['code'], r'接口返回[code]测试不通过')
+
+        return self.test_data
 
 
 if __name__ == '__main__':
-    # a =UnionMainSweepTest()
-    # print(a.load_test_data())
-    # unittest.main()
-    conn = DBUtil.get_connection(DBName.CHANJET_PAY_CHANNEL, DBENV.STABLE)
-    cur = conn.cursor()
-    cur.execute(r"select * from cp_chn_pay_trans where id = '3354'")
-    result = cur.fetchall()
-    print(result)
-    a = list(result[0])
-    print(a)
-    b = json.loads({"channel": a})
-    print(b)
-    cur.close()
-    conn.close()
+    unittest.main()
